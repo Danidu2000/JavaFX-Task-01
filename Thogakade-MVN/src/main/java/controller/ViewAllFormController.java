31e3308e483ea21e91181e53f9f57491ff3842a0
Danidu2000 <daniduparamuditha2000@gmail.com>
2024-08-17 14:58:43 +0530

    Complete the view page functions.

--- Thogakade-MVN/src/main/java/controller/ViewAllFormController.java
+++ Thogakade-MVN/src/main/java/controller/ViewAllFormController.java
@@ -1,12 +1,14 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import db.ThogakadePOS;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.Customer;
 
@@ -15,6 +17,22 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class ViewAllFormController implements Initializable {
+
+    private List<Customer> customerList = ThogakadePOS.getInstance().getConnection();
+
+    private Customer foundCustomer = null;
+
+    private Customer foundCustomerForProcess = null;
+
+    @FXML
+    private JFXButton btnUpdateCustomer;
+
+    @FXML
+    private JFXButton btnDeleteCustomer;
+
+
+    @FXML
+    private JFXComboBox<String> cmbTitle;
 
     @FXML
     private TableColumn<?, ?> colAddress;
@@ -35,23 +53,138 @@ public class ViewAllFormController implements Initializable {
     private TableColumn<?, ?> colTitle;
 
     @FXML
+    private DatePicker dateDob;
+
+    @FXML
+    private Label lblID;
+
+    @FXML
     private TableView<Customer> tblCustomer;
 
     @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtNumber;
+
+    @FXML
+    private JFXTextField txtSearchID;
+
+    @FXML
+    void btnDeleteCustomerOnActon() {
+        customerList.remove(foundCustomerForProcess);
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Delete Success");
+        alert.setHeaderText("SUCCESS!");
+        alert.setContentText("The customer successfully deleted.");
+        alert.showAndWait();
+
+        txtSearchID.setText("");
+        lblID.setText(null);
+        cmbTitle.setValue(null);
+        txtName.setText(null);
+        txtAddress.setText(null);
+        txtNumber.setText(null);
+        dateDob.setValue(null);
+
+        btnDeleteCustomer.setDisable(true);
+
+        tblCustomer.refresh();
+        loadTable();
+    }
+
+    @FXML
     void btnReloadOnAction() {
-        colID.setCellValueFactory(new PropertyValueFactory<>("id"));
-        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
-        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
-        colNumber.setCellValueFactory(new PropertyValueFactory<>("number"));
-        colDob.setCellValueFactory(new PropertyValueFactory<>("dob"));
+        loadTable();
+        tblCustomer.refresh();
+    }
 
-        List<Customer> customerList = ThogakadePOS.getInstance().getConnection();
-        ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
+    @FXML
+    void btnSearchOnAction() {
+        String id = txtSearchID.getText();
 
-        customerObservableList.addAll(customerList);
+        for (Customer customer : customerList) {
+            if (customer.getId().equals(id)) {
+                foundCustomer = customer;
+                foundCustomerForProcess = customer;
+                break;
+            }
+        }
+        if ("".equals(id)) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Empty Search");
+            alert.setHeaderText("Your search did not return any results!");
+            alert.setContentText("please enter valid customer ID.");
+            alert.showAndWait();
 
-        tblCustomer.setItems(customerObservableList);
+            lblID.setText(null);
+            cmbTitle.setValue(null);
+            txtName.setText(null);
+            txtAddress.setText(null);
+            txtNumber.setText(null);
+            dateDob.setValue(null);
+
+        }else if (foundCustomer != null) {
+            lblID.setText(foundCustomer.getId());
+            cmbTitle.setValue(foundCustomer.getTitle());
+            txtName.setText(foundCustomer.getName());
+            txtAddress.setText(foundCustomer.getAddress());
+            txtNumber.setText(foundCustomer.getNumber());
+            dateDob.setValue(foundCustomer.getDob());
+
+            foundCustomer = null;
+
+            btnUpdateCustomer.setDisable(false);
+            btnDeleteCustomer.setDisable(false);
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Search Error");
+            alert.setHeaderText("Customer with ID '" + id + "' not found!");
+            alert.setContentText("please check the ID or search different ID.");
+            alert.showAndWait();
+
+            txtSearchID.setText("");
+            lblID.setText(null);
+            cmbTitle.setValue(null);
+            txtName.setText(null);
+            txtAddress.setText(null);
+            txtNumber.setText(null);
+            dateDob.setValue(null);
+
+        }
+    }
+
+    @FXML
+    void btnUpdateCustomerOnAction() {
+        foundCustomerForProcess.setTitle(cmbTitle.getValue());
+        foundCustomerForProcess.setName(txtName.getText());
+        foundCustomerForProcess.setAddress(txtAddress.getText());
+        foundCustomerForProcess.setNumber(txtNumber.getText());
+        foundCustomerForProcess.setDob(dateDob.getValue());
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Update Success");
+        alert.setHeaderText("SUCCESS!");
+        alert.setContentText("The customer successfully updated.");
+        alert.showAndWait();
+
+        txtSearchID.setText("");
+        lblID.setText(null);
+        cmbTitle.setValue(null);
+        txtName.setText(null);
+        txtAddress.setText(null);
+        txtNumber.setText(null);
+        dateDob.setValue(null);
+
+        btnUpdateCustomer.setDisable(true);
+
+        tblCustomer.refresh();
+        loadTable();
     }
 
     @Override
@@ -63,7 +196,54 @@ public class ViewAllFormController implements Initializable {
         colNumber.setCellValueFactory(new PropertyValueFactory<>("number"));
         colDob.setCellValueFactory(new PropertyValueFactory<>("dob"));
 
-        List<Customer> customerList = ThogakadePOS.getInstance().getConnection();
+        ObservableList<String> titles = FXCollections.observableArrayList();
+        titles.add("Mr.");
+        titles.add("Mrs.");
+        titles.add("Miss.");
+
+        cmbTitle.setItems(titles);
+
+        loadTable();
+
+        btnUpdateCustomer.setDisable(true);
+        btnDeleteCustomer.setDisable(true);
+
+        tblCustomer.getSelectionModel().selectedItemProperty().addListener(((observableValue, oldValue, newValue) -> setTextToValues(newValue)));
+
+    }
+
+    private void setTextToValues(Customer newValue) {
+        if (newValue != null) {
+            foundCustomer = newValue;
+            foundCustomerForProcess = newValue;
+
+            txtSearchID.setText(newValue.getId());
+            lblID.setText(newValue.getId());
+            cmbTitle.setValue(newValue.getTitle());
+            txtName.setText(newValue.getName());
+            txtAddress.setText(newValue.getAddress());
+            txtNumber.setText(newValue.getNumber());
+            dateDob.setValue(newValue.getDob());
+
+            btnUpdateCustomer.setDisable(false);
+            btnDeleteCustomer.setDisable(false);
+        } else {
+            clearFields();
+        }
+    }
+
+    private void clearFields() {
+        txtSearchID.setText("");
+        lblID.setText(null);
+        cmbTitle.setValue(null);
+        txtName.setText(null);
+        txtAddress.setText(null);
+        txtNumber.setText(null);
+        dateDob.setValue(null);
+    }
+
+    private void loadTable(){
+        customerList = ThogakadePOS.getInstance().getConnection();
         ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
 
         customerObservableList.addAll(customerList);
